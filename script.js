b8f034c190573660c6e6d6cf44729bacb1bf32df
Kallepalli Naveen <159025717+naveenkallepalli7@users.noreply.github.com>
2024-07-24 16:47:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+const topButton = document.getElementById('top-button');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        topButton.style.display = 'block';
+    } else {
+        topButton.style.display = 'none';
+    }
+});
+
+topButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
